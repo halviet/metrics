2c41b7e2a7fe28f1ebc55fa1685648a017ad1802
Egor Kramer <invscrl@gmail.com>
2024-08-04 19:56:21 +0700

    add: chi router; GET value; all metrics HTML page

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
 	"github.com/halviet/metrics/internal/handlers"
 	"github.com/halviet/metrics/internal/storage"
 	"log"
@@ -10,11 +11,13 @@ import (
 var store = storage.New()
 
 func main() {
-	mux := http.NewServeMux()
+	r := chi.NewRouter()
 
-	mux.HandleFunc("POST /update/{metricType}/{metricName}/{metricValue}", handlers.MetricHandler(store))
+	r.Post("/update/{metricType}/{metricName}/{metricValue}", handlers.MetricHandler(store))
+	r.Get("/value/{metricType}/{metricName}", handlers.GetMetricHandle(store))
+	r.Get("/", handlers.GetAllMetricsPageHandler(store))
 
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal(err)
 	}
 }
